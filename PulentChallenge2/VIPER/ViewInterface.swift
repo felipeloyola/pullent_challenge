0dd6c924bb2ca1491af7025e1a6f8d5a815cfd68
Felipe Loyola <felipe.l@funxperience.com>
2020-07-28 21:33:16 -0400

    Add messages utility

--- PulentChallenge2/VIPER/ViewInterface.swift
+++ PulentChallenge2/VIPER/ViewInterface.swift
@@ -1,5 +1,14 @@
+import UIKit
+
+
 protocol ViewInterface: class {
 }
 
 extension ViewInterface {
+    func createAlert(with title: String?, message: String?) -> UIAlertController {
+        let okAction = UIAlertAction(title: "OK", style: .default, handler: nil)
+        let alert = UIAlertController(title: title, message: message, preferredStyle: .alert)
+        alert.addAction(okAction)
+        return alert
+    }
 }
